a891ee1d67b7d9e11f61078108179def773449ca
Gilbert Koomson <gilbertkoomson00@gmail.com>
2024-12-08 05:24:52 -0800

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import Item from "./components/product"
 import Cart from "./components/cart"
 import waffle from './assets/image-waffle-mobile.jpg'
 import creme from './assets/image-creme-brulee-mobile.jpg'
@@ -16,22 +15,31 @@ import data from "./data"
 function App() {
 
   const [items,setItems]=useState(data)
+
+	function addToCart(userId){
+		setItems()
+	}
+
 	const foods=items.map(
 		(item)=><li key={item.id}>
 			<div>
 				<div>
-					{
-						item.id===0 ? (
-							<img src={Cotta}/>
-						):(<p></p>)
-					}
+          {item.id===0 ? <img src={waffle} alt="" /> : <p></p>}
+          {item.id===1 ? <img src={creme} alt="" /> : <p></p>}
+          {item.id===2 ? <img src={macaron} alt="" /> : <p></p>}
+          {item.id===3 ? <img src={Tiramisu} alt="" /> : <p></p>}
+          {item.id===4 ? <img src={Baklava} alt="" /> : <p></p>}
+          {item.id===5 ? <img src={Pie} alt="" /> : <p></p>}
+          {item.id===6 ? <img src={Cake} alt="" /> : <p></p>}
+          {item.id===7 ? <img src={Brownie} alt="" /> : <p></p>}
+          {item.id===8 ? <img src={Cotta} alt="" /> : <p></p>}
+
 				</div>
-				<img src={macaron} alt="" />
 				<button>Add to Cart</button>
 				<p>{item.number}</p>
 			</div>
 			<div>
-				<p>{item.category}</p>
+				<p>{item.category}</p>   
 				<p>{item.name}</p>
 				<p>{item.price}</p>
 			</div>
@@ -41,71 +49,9 @@ function App() {
 
   return (
     <>
-      <p>Deserts</p>
-			<ul>{foods}</ul>
-      <Item
-        name={'Waffle'}
-        image={waffle}
-        category={'Desserts Waffle with Berries'}
-        price={'$6.50'}
-
-      />
-      <Item
-        name={'Crème Brûlée'}
-        image={creme}
-        category={'Vanilla Bean Crème Brûlée'}
-        price={'$7.50'}
-
-      />
-      <Item
-        name={'Macaron'}
-        image={macaron}
-        category={'Macaron Mix of Five'}
-        price={'$8.00'}
-
-      />
-      <Item
-        name={'Tiramisu'}
-        image={Tiramisu}
-        category={'Classic Tiramisu'}
-        price={'$5.50'}
-
-      />
-      <Item
-        name={'Baklava'}
-        image={Baklava}
-        category={'Pistachio Baklava'}
-        price={'$4.00'}
-
-      />
-      <Item
-        name={'Pie'}
-        image={Pie}
-        category={'Lemon Meringue Pie'}
-        price={'$5.00'}
-
-      />
-      <Item
-        name={'Cake'}
-        image={Cake}
-        category={'Red Velvet Cake'}
-        price={'$4.50'}
-
-      />
-      <Item
-        name={'Brownie'}
-        image={Brownie}
-        category={'Salted Caramel Brownie'}
-        price={'$4.50'}
-
-      />
-      <Item
-        name={'Cotta'}
-        image={Cotta}
-        category={'Vanilla Panna Cotta Panna'}
-        price={'$6.50'}
-
-      />
+      <ul>
+        {foods}
+      </ul>
 
       <Cart someItems={items}/>
     </>
