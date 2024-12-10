8cc70d6b34a89c62b73b7b159823d469ffe71ea5
Gilbert Koomson <gilbertkoomson00@gmail.com>
2024-12-09 20:15:52 -0800

    finishe modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import Cart from "./components/cart"
 import waffle from './assets/image-waffle-mobile.jpg'
 import creme from './assets/image-creme-brulee-mobile.jpg'
 import macaron from './assets/image-macaron-mobile.jpg'
@@ -11,13 +10,37 @@ import Pie from './assets/image-meringue-mobile.jpg'
 import { useState } from "react"
 import data from "./data"
 import Remover from './assets/icon-remove-item.svg'
+import Carbon from './assets/icon-carbon-neutral.svg'
 import Increment from './assets/icon-increment-quantity.svg'
 import Decrement from './assets/icon-decrement-quantity.svg'
+import Empty from './assets/illustration-empty-cart.svg'
+import { Button } from './components/ui/button'
+
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
 
 
 function App() {
 
   const [items,setItems]=useState(data)
+	const [isCartEmpty,setIsCartEmpty]=useState(true)
+
 
   const removeFromCart = (id) => {
     setItems(items.map(product => 
@@ -27,6 +50,9 @@ function App() {
 
 
 	const totalItemsInCart= items[0].number+items[1].number+items[2].number+items[3].number+items[4].number+items[5].number+items[6].number+items[7].number+items[8].number
+	const orderTotal= (items[0].number*items[0].price)+(items[1].number*items[1].price)+(items[2].number*items[2].price)+(items[3].number*items[3].price)+(items[4].number*items[4].price)+(items[5].number*items[5].price)+(items[6].number*items[6].price)+(items[7].number*items[7].price)+(items[8].number*items[8].price)
+	
+
 
 
   const filteredItems = items
@@ -43,9 +69,34 @@ function App() {
       </li>
     ));
 
+		const dialogueContent = items
+    .filter(item => item.number > 0)
+    .map(item => (
+      <li key={item.id}>
+				<div>
+          {item.id===0 ? <img src={waffle} alt="" /> : <p></p>}
+          {item.id===1 ? <img src={creme} alt="" /> : <p></p>}
+          {item.id===2 ? <img src={macaron} alt="" /> : <p></p>}
+          {item.id===3 ? <img src={Tiramisu} alt="" /> : <p></p>}
+          {item.id===4 ? <img src={Baklava} alt="" /> : <p></p>}
+          {item.id===5 ? <img src={Pie} alt="" /> : <p></p>}
+          {item.id===6 ? <img src={Cake} alt="" /> : <p></p>}
+          {item.id===7 ? <img src={Brownie} alt="" /> : <p></p>}
+          {item.id===8 ? <img src={Cotta} alt="" /> : <p></p>}
+
+				</div>
+        <p>{item.name}</p>
+        <p>
+          &#215; {item.number} @ ${item.price} = ${item.number * item.price}
+        </p>
+      </li>
+    ));
+
+
 
 
 	const handleAddToCart = (id) => {
+		setIsCartEmpty(false)
     // Update the number for the item with the given id
     setItems((prevItems) =>
       prevItems.map((item) =>
@@ -100,12 +151,50 @@ function App() {
 
   return (
     <>
-      <ul className="bg-gray-200 ml-96">
+      <ul className="bg-gray-200">
         {foods}
       </ul>
 
-      <p>Carts({totalItemsInCart})</p>
-      <ul>{filteredItems}</ul>
+			<div>
+				<p>Carts({totalItemsInCart})</p>
+				<div>
+					{
+						isCartEmpty ?
+						<div>
+							<img src={Empty} alt="" />
+							<p>Your added items will appear here</p>
+						</div>:
+						<div className="bg-red-950">
+							<ul>{filteredItems}</ul>
+							<p>Order Total ${orderTotal}</p>
+							<p> <img src={Carbon} alt="" /> This carbon neutral delivery</p>
+							<AlertDialog>
+								<AlertDialogTrigger>Confirm Order</AlertDialogTrigger>
+								<AlertDialogContent>
+									<AlertDialogHeader>
+										<AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+										<AlertDialogDescription>
+											{dialogueContent}
+											<div>
+												<p>Order Total</p>
+												<p>${orderTotal}</p>
+											</div>
+										</AlertDialogDescription>
+									</AlertDialogHeader>
+									<AlertDialogFooter>
+										
+										<AlertDialogAction>Start New Order</AlertDialogAction>
+									</AlertDialogFooter>
+								</AlertDialogContent>
+							</AlertDialog>
+						</div>
+
+					}
+
+				</div>
+			</div>
+
+
     </>
   )
 }
